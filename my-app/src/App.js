f6faa68bcf3a4f0aa43bcdc52b12156f260827fd
pakkONE <johan.pakarinen@gmail.com>
2022-07-03 11:31:50 +0000

    Initial Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+export default App;
